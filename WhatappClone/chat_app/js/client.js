5bedc5a02ffb7d045497cf2a01c919261d727af8
rimjhim065 <rimjhim0586.be20@chitkara.edu.in>
2023-04-24 00:14:40 +0530

    initial commit

---
+++ WhatappClone/chat_app/js/client.js
@@ -0,0 +1,51 @@
+const socket = io('http://localhost:8000');
+
+// Get DOM elements in respective Js variables
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+
+// Audio that will play on receiving messages
+var audio = new Audio('ting.mp3');
+
+// Function which will append event info to the contaner
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position =='left'){ 
+        audio.play();
+    }
+}
+
+
+// Ask new user for his/her name and let the server know
+const n = prompt("Enter your name to join");
+console.log(n);
+socket.emit('new-user-joined', n);
+
+// If a new user joins, receive his/her name from the server
+socket.on('user-joined', n =>{
+    append(`${n} joined the chat`, 'right')
+})
+
+// If server sends a message, receive it
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+// If a user leaves the chat, append the info to the container
+socket.on('left', n =>{
+    append(`${n} left the chat`, 'right')
+})
+
+// If the form gets submitted, send server the message
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
+})
